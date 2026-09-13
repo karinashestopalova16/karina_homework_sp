7c9f03c3cfdc6092a872e32f56ca6855a2a68559
Karina Shestopalova <skarishestopalowa16@yandex.ru>
2026-09-13 16:53:09 +0300

    New function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,10 @@ def filter_by_state(list_of_operations: list[dict], state: str = "EXECUTED") -> 
             final_operations_list.append(operation)
 
     return final_operations_list
+
+
+def sort_by_date(list_of_operations: list[dict], reverse: bool = True) -> list[dict]:
+    """Функция, которая сортирует список операций по дате операции"""
+    final_sorted_list = sorted(list_of_operations, key=lambda operation: operation["date"], reverse=reverse)
+
+    return final_sorted_list
