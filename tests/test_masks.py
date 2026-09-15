381119c22955ca4eebe7214fcbb2137de0d03f1b
Karina Shestopalova <skarishestopalowa16@yandex.ru>
2026-09-15 17:40:13 +0300

    Add tests for masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,26 +3,41 @@ import pytest
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_valid_card_number() -> None:
-    result = get_mask_card_number("7000792289606361")
-    assert result == "7000 79** **** 6361"
+# тест маскировки номера карты
+@pytest.mark.parametrize(
+    "card_number, expected",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("1234567890123456", "1234 56** **** 3456"),
+    ],
+)
+def test_card_mask(card_number: str, expected: str) -> None:
+    result = get_mask_card_number(card_number)
+    assert result == expected
 
 
-def test_get_mask_another_card_number() -> None:
-    result = get_mask_card_number("1234567890123456")
-    assert result == "1234 56** **** 3456"
-
-
-def test_get_mask_invalid_card_number() -> None:
+# тест выбрасывания ошибки при некорректном вводе номера карты
+@pytest.mark.parametrize("invalid_card_number", ["1234", "", "12345678912345678912345"])
+def test_invalid_card_mask(invalid_card_number: str) -> None:
     with pytest.raises(ValueError):
-        get_mask_card_number("1234")
+        get_mask_card_number(invalid_card_number)
 
 
-def test_get_mask_valid_account() -> None:
-    result = get_mask_account("73654108430135874305")
-    assert result == "**4305"
+# тест маскировки номера счета
+@pytest.mark.parametrize(
+    "account_number, expected",
+    [
+        ("73654108430135874305", "**4305"),
+        ("1234", "**1234"),
+    ],
+)
+def test_get_account_mask(account_number: str, expected: str) -> None:
+    result = get_mask_account(account_number)
+    assert result == expected
 
 
-def test_get_mask_invalid_account() -> None:
+# тест выбрасывания ошибки при некорректном вводе номера счета
+@pytest.mark.parametrize("invalid_account_number", ["123", ""])
+def test_invalid_account_number_mask(invalid_account_number: str) -> None:
     with pytest.raises(ValueError):
-        get_mask_account("123")
+        get_mask_account(invalid_account_number)
